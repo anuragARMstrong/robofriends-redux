0fd648d3646e4146029b808aaff175e4ec20e21c
anuragARMstrong <anurag.armstrong@gmail.com>
2021-12-29 16:43:01 +0530

    added redux middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import { searchRobots } from './reducers';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 // <Provider> is a component which passes redux store to all wrapped components,
 // without manually passing store as props to some component
